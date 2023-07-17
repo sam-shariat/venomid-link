1bfefa90aaf82ea79010be12c4de3b015a541483
Sam Shariat <samshariat7@gmail.com>
2023-07-17 20:44:48 +0330

    using appkit

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,12 +1,39 @@
+import { CONTRACT_ADDRESS } from 'core/utils/constants';
 import type { NextApiRequest, NextApiResponse } from 'next';
+const { TonClient, signerKeys } = require('@eversdk/core');
+const { libNode } = require('@eversdk/lib-node');
+const { Account } = require('@eversdk/appkit');
+const { CollectionContract } = require('abi/CollectionContract');
 
 type Data = {
   name: string;
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'Rcell Dealer' });
+(async () => {
+  TonClient.useBinaryLibrary(libNode);
+})();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const client = new TonClient({
+    network: {
+      endpoints: ['https://gql-testnet.venom.foundation/graphql']
+    },
+  });
+  try {
+
+    const keys = await client.crypto.generate_random_sign_keys();
+
+    const collection = new Account(CollectionContract, {
+        signer: signerKeys(keys),
+        client,
+        address: CONTRACT_ADDRESS
+    });
+
+    let response = await collection.run("getInfoByName", { name: 'sam'});
+    res.status(200).json(response);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  client.close();
 }
