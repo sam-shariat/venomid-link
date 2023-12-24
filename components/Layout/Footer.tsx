2704b5dd1d9beaaf06b0c7e1bcc999ac4558ac8b
sam <samshariat7@gmail.com>
2023-12-24 22:34:10 +0330

    fixing footer color

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -5,13 +5,29 @@ import { roundAtom, lightModeAtom } from 'core/atoms';
 import { useAtomValue } from 'jotai';
 export default function Footer() {
   const round = useAtomValue(roundAtom);
-  const {colorMode} = useColorMode();
+  const lightMode = useAtomValue(lightModeAtom);
+  const { colorMode } = useColorMode();
+
   return (
-    <Box as="footer" width={['100%', '100%', 'md', 'lg', 'xl']} py={4} color={colorMode === 'light' ? 'var(--dark)' : 'var(--light)'} backgroundColor={colorMode === 'light' ? 'whiteAlpha.200' : 'blackAlpha.200'} my={12} rounded={round}>
-      <Container gap={1} maxW="container.md" display="flex" justifyContent="center" alignItems={'center'}>
+    <Box
+      as="footer"
+      width={['100%', '100%', 'md', 'lg', 'xl']}
+      py={4}
+      color={lightMode ? 'var(--dark)' : 'var(--light)'}
+      backgroundColor={lightMode ? 'whiteAlpha.200' : 'blackAlpha.200'}
+      my={12}
+      rounded={round}>
+      <Container
+        gap={1}
+        maxW="container.md"
+        display="flex"
+        justifyContent="center"
+        alignItems={'center'}>
         <Text>Powered By</Text>
         <Logo />
-        <Link id="venomid-app-github" href={SITE_CLAIM_URL}>VenomID</Link>
+        <Link id="venomid-app-github" href={SITE_CLAIM_URL}>
+          VenomID
+        </Link>
       </Container>
     </Box>
   );
